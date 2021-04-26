142327e1502a65bb99d937cc31c9fed37e4cfc81
Kiyas <kiiyas@yandex.ru>
2021-04-26 20:18:42 +0500

    3 done

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,3 +1,5 @@
+'use strict';
+
 let count = document.getElementById('dead');
 let defeat = document.getElementById('lost');
 
@@ -8,25 +10,26 @@ for (let i = 1; i <= 9; i++) {
     hole.onclick = function () {    
         if (hole.className.includes( 'hole_has-mole' )) {
             count.textContent ++;
-            console.log(defeat.textContent) 
+            if (count.textContent == 10) {
+                alert('ПОБЕДА!');
+                count.textContent = 0; 
+                defeat.textContent = 0; 
+            }
         }
 
         if (!hole.className.includes( 'hole_has-mole' )) {
-        defeat.textContent ++;
-        console.log(defeat.textContent)
+            defeat.textContent ++;
+            if (defeat.textContent == 5) {
+                alert('КРОТЫ ПОБЕДИЛИ...');
+                count.textContent = 0; 
+                defeat.textContent = 0;     
+            }
         }    
-    };
-
-    hole.onclick();
-    /*
-    if (defeat.textContent = 5) {
-        alert('КРОТЫ ПОБЕДИЛИ...')    
-    }
-    
-    if (count.textContent = 10) {
-        alert('ПОБЕДА!')    
-    }
-    */
+    };     
 }
+
+
+
+
 
 
