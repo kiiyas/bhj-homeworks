a20fde6ee4a3b31b3012fcea69b2f3977ff79257
Kiyas <kiiyas@yandex.ru>
2021-05-03 23:17:56 +0500

    fixed

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -3,40 +3,23 @@
 const mainPopup = document.getElementById('modal_main');
 const successPopup = document.getElementById('modal_success');
 const popupСlose = document.getElementsByClassName('modal__close');
+const showSuccess = document.getElementsByClassName('show-success');
+let closeTimes = popupСlose.length;
 
 mainPopup.classList.add('modal_active'); 
 
-let closeTimes = popupСlose.length; 
+for (let i = 0; i < closeTimes; i++) {
+    popupСlose[i].onclick = function() {
+        mainPopup.classList.remove('modal_active');
+        successPopup.classList.remove('modal_active');
+    }
+}
+
+showSuccess[0].onclick = function() {
+    mainPopup.classList.remove('modal_active');
+    successPopup.classList.add('modal_active');
+}
+
+
 //? для любого количества модалок - заменить mainPopup и successPopup на 
 //? один document.getElementsByClassName('modal'), и подбирать по [0], [1] ...
-for (let i = 0; i < closeTimes; i++) {
-    if (i == 0 || i == 1 || i % 2 == 1) {
-        popupСlose[i].onclick = function() {
-            mainPopup.classList.remove('modal_active');
-            successPopup.classList.add('modal_active');     
-        }
-    } else {
-        popupСlose[i].onclick = function() {
-            successPopup.classList.remove('modal_active');         
-        }
-    }
-}  
-
-//себе в помощь:
-/*
-
-popupСlose[0].onclick = function() {
-    mainPopup.classList.remove('modal_active');
-    successPopup.classList.add('modal_active');     
-}
-
-popupСlose[1].onclick = function() {
-    mainPopup.classList.remove('modal_active');
-    successPopup.classList.add('modal_active');     
-}
-
-popupСlose[2].onclick = function() {
-    successPopup.classList.remove('modal_active');         
-}
-
-*/
