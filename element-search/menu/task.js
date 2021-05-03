57a59b1fefdb23c3633c57f42205cfedcad2e953
Kiyas <kiiyas@yandex.ru>
2021-05-03 23:43:22 +0500

    fixed

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,8 +1,29 @@
 'use strict'
 
+const mainMenuItems = document.querySelectorAll('.menu__link');
+
+for (let i = 0; i < mainMenuItems.length; i++ ) {
+    let haveDropMenu = mainMenuItems[i].closest('.menu__item').querySelector('ul.menu_sub');
+    if(haveDropMenu) { 
+        mainMenuItems[i].addEventListener('click', function (event) {
+        event.preventDefault();
+
+        const dropMenuActive = document.querySelector('.menu_active');
+            
+        if (haveDropMenu.classList.contains('menu_active')) {
+                haveDropMenu.classList.remove('menu_active')
+            } else if (dropMenuActive) {
+                dropMenuActive.classList.remove('menu_active');
+                haveDropMenu.classList.add('menu_active');
+            } else {
+                haveDropMenu.classList.add('menu_active')
+            };            
+            })
+    }
+}
+
+/*
 const mainMenuItems = document.getElementsByClassName('menu__link');
-//const menuActive = document.querySelector('.menu_active');
-//если присваивать, то перестает работать переключатель в строке 14-15 - почему?
 
 let mainMenuLength = mainMenuItems.length;
 for (let i = 0; i < mainMenuLength; i++) {    
@@ -19,3 +40,4 @@ for (let i = 0; i < mainMenuLength; i++) {
         }        
     };         
 }
+*/
