536aa56df75324bfce67a71a4141c4a7978d28f8
Kiyas <kiiyas@yandex.ru>
2021-05-05 23:09:24 +0500

    final fixes

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -10,16 +10,26 @@ mainPopup.classList.add('modal_active');
 
 for (let i = 0; i < closeTimes; i++) {
     popupСlose[i].onclick = function() {
-        mainPopup.classList.remove('modal_active');
-        successPopup.classList.remove('modal_active');
+        this.closest('.modal').classList.remove('modal_active');
+        //коммент преподавателя:
+        //Так функция более универсальна и закрывает любое одно окно 
+        //к которому принадлежит элемент закрытия
+        
+        //mainPopup.classList.remove('modal_active');
+        //successPopup.classList.remove('modal_active');
     }
 }
-
+//!коммент преподавателя:
+//Например можно было сделать, чтобы на кнопке “Сделать хорошо” срабатывало два обработчика: 
+//один будет закрывать текущее модальное окно (который вы реализовали выше), а второй 
+//будет отображать второе модальное окно.
+showSuccess[0].addEventListener('click', () => successPopup.classList.add('modal_active'));
+/*
 showSuccess[0].onclick = function() {
     mainPopup.classList.remove('modal_active');
     successPopup.classList.add('modal_active');
 }
-
+*/
 
 //? для любого количества модалок - заменить mainPopup и successPopup на 
 //? один document.getElementsByClassName('modal'), и подбирать по [0], [1] ...
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,11 +1,15 @@
 'use strict'
 
+/*
 const sliderItems = document.querySelectorAll('.slider__item');
 const prev = document.querySelector('.slider__arrow_prev');
 const next = document.querySelector('.slider__arrow_next');
 
 const sliderDots = document.getElementsByClassName('slider__dot');
 const sliderDotsLength = sliderDots.length;
+
+const position = document.getElementsByClassName('slider__item_active');
+
 
 next.addEventListener('click', function () {
     let arr = Array.from(sliderItems)
@@ -34,8 +38,7 @@ prev.addEventListener('click', function () {
 function hasActive (e) {
     return e.classList.contains('slider__item_active');
 }
-
-
+*/
 /*
 
   for (let i = 0; i < sliderDotsLength; i++) {
@@ -54,3 +57,53 @@ function hasActive (e) {
     }
 }
 */
+
+
+//!коммент преподавателя:
+//Представьте, что вам в дальнейшем придётся расширять слайдер, например управлять им 
+//по интервалу, по клику на клавиатуру или ещё иными действиями (например скролу колёсика мышки). 
+//Все обработчики вам придётся реализовывать независимо, что будет сильно расширять программу. 
+//В ней легко можно будет запутаться, а расширение будет не удобным.
+
+//Попробуйте управление вынести в отдельную функцию. Напишите функцию, которая 
+//аргументом принимает число и выставляет этот слайд.
+
+const sliderItems = document.querySelectorAll('.slider__item');
+
+const sliderDots = document.getElementsByClassName('slider__dot');
+const prev = document.querySelector('.slider__arrow_prev');
+const next = document.querySelector('.slider__arrow_next');
+
+let slideArr = Array.from(sliderItems);
+let position = slideArr.findIndex(hasActive);
+
+next.addEventListener('click', function () {
+    position == (slideArr.length - 1) ? position = 0 : position += 1;
+    getStep (position);
+})
+
+prev.addEventListener('click', function () {
+    position == 0 ? position = (slideArr.length - 1) : position -= 1;
+    getStep (position);
+})
+
+for (let i = 0; i < sliderDots.length; i++) {
+    sliderDots[i].addEventListener('click', function () {
+        //position = та точка, по которой кликнули;
+        getStep (position);        
+    })
+
+
+function getStep (position) { //передать сюда номер позиции в зависимости от нажатой кнопки    
+    let index = slideArr.findIndex(hasActive);
+
+    sliderItems[index].classList.remove('slider__item_active');
+    sliderDots[index].classList.remove('slider__dot_active');
+    
+    sliderItems[position].classList.add('slider__item_active');  //активировать слайд по переданной позиции
+    sliderDots[position].classList.add('slider__dot_active');
+}
+
+function hasActive (e) {
+    return e.classList.contains('slider__item_active');
+}
