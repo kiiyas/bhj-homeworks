50993725d45dc2709ec3719745e40088c3f262a7
Kiyas <kiiyas@yandex.ru>
2021-06-28 18:19:30 +0500

    fixed

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,22 +1,34 @@
-'use strict';
+'use strict'
 
-const hasTooltip = document.getElementsByClassName('has-tooltip');
+const hasTooltip = document.querySelectorAll('a.has-tooltip');
+//const tooltipDiv = document.querySelector('div.tooltip'); // если присваивать, всегда фолсит в клике
 
-for (let item of hasTooltip) {
-    const tooltip = document.createElement('div');
-    tooltip.className = 'tooltip';
-    tooltip.innerHTML = item.title;
-    
-    tooltip.style.cssText = `position: absolute; left: ${item.getBoundingClientRect().left}px; top: ${item.getBoundingClientRect().top + 20}px`;
-    item.append(tooltip);
-
-    item.addEventListener('click', (e) => {
-        e.preventDefault();
-        const active = document.getElementsByClassName('tooltip_active')[0];
-        if (active) {
-            active.classList.remove('tooltip_active');
-        }       
-
-        tooltip.classList.add('tooltip_active');        
-    });
+//подсказка удалится при клике на другую подсказу и на любое свободное место:
+function hideActiveTips() {
+    const active = document.getElementsByClassName('tooltip_active');
+    if (active[0]) {
+         active[0].classList.remove('tooltip_active');
+     }
 }
+
+function createTip(item) {
+    const tooltip = document.createElement('div');    
+    tooltip.innerHTML = '<div class="tooltip tooltip_active">' + item.title + '</div>';
+    tooltip.firstChild.style.cssText = `position: absolute; left: ${item.getBoundingClientRect().left}px; top: ${item.getBoundingClientRect().top + 20}px`;
+    
+    return tooltip.firstChild;
+}
+
+window.addEventListener('click', (e) => {
+    hideActiveTips();
+    for (let element of hasTooltip) {
+        e.preventDefault();
+
+        if (e.target == element && element.nextSibling == document.querySelector('div.tooltip')) { 
+            document.querySelector('div.tooltip').remove();
+        } else if (e.target == element) {
+            const tooltip = createTip(element);
+            element.insertAdjacentElement('afterEnd', tooltip);
+        }       
+    }
+})
